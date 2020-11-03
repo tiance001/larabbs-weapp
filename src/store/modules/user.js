12d2dee4cecca132906ade0df910c41a051d0a31
tiance001 <1340618494@qq.com>
2020-11-03 15:05:25 +0800

    register

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,5 +1,5 @@
 import wepy from '@wepy/core'
-import { login, logout, refresh } from '@/api/auth'
+import { login, logout, refresh, register } from '@/api/auth'
 import { getCurrentUser } from '@/api/user'
 import * as auth from '@/utils/auth'
 import isEmpty from 'lodash/isEmpty'
@@ -55,6 +55,14 @@ const actions = {
     // 清空 storage
     auth.logout()
     commit('resetState')
+  },
+  async register ({ dispatch }, params = {}) {
+    const loginData = await wepy.wx.login()
+    params.code = loginData.code
+
+    await register(params)
+
+    await dispatch('login')
   }
 }
 
